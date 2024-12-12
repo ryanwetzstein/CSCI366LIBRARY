cda45e31cf2128b5cba5d30feda6a8a1fff75245
rwetz <rwetz00@gmail.com>
2024-12-12 12:31:44 -0600

    test

--- src/pkg366libraryapp/Main.java
+++ src/pkg366libraryapp/Main.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg366libraryapp;
 
 /**
  *
- * @author rwetz
+ * @author group
  */
 public class Main {
 
@@ -14,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("test");
+        System.out.println("penis");
     }
-    
 }
