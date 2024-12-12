37be4a454ec32599f70dfd7ac2045abfbd0d92bf
rwetz <rwetz00@gmail.com>
2024-12-12 12:59:05 -0600

    Added stuff

--- src/pkg366libraryapp/Main.java
+++ src/pkg366libraryapp/Main.java
@@ -1,18 +1,218 @@
 package pkg366libraryapp;
 
+import java.sql.*;
+import java.util.Scanner;
+
 /**
  *
  * @author group
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("penis");
-        System.out.println("big dawg");
-        System.out.println("nashat was here");
+    public class DatabaseManager {
+
+        private static String jdbcURL = "jdbc:postgresql://localhost:5432/LibraryAdmin";
+        private static String username = "postgres";
+        private static String password = "password12345!";
+
+        private static Connection connection;
+
+        public static Connection getConnection() {
+            if (connection == null) {
+                setConnection();
+            }
+            return connection;
+        }
+
+        private static void setConnection() {
+            try {
+                // load the driver
+                Class.forName("org.postgresql.Driver");
+                // connect to the database
+                connection
+                        = DriverManager.getConnection(jdbcURL, username, password);
+
+            } catch (ClassNotFoundException e) {
+                System.out.println("Cannot load the driver");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println("Got a SQL exception.");
+                e.printStackTrace();
+            }
+        }
     }
+
+    public class Department {
+
+        private int dept_number;
+        private String dept_name;
+
+        public Department() {
+        }
+
+        public Department(String dept_name) {
+            this.dept_name = dept_name;
+        }
+
+        public Department(int dept_number, String dept_name) {
+            this.dept_number = dept_number;
+            this.dept_name = dept_name;
+        }
+
+        public static Department getDepartmentByName(String deptname) throws SQLException {
+
+            String select = "SELECT * FROM department "
+                    + " where department_name = ?";
+
+            PreparedStatement stmtSelect = DatabaseManager.getConnection().prepareStatement(select);
+            stmtSelect.setString(1, deptname);
+            ResultSet rsOne = stmtSelect.executeQuery();
+
+            Department dept = null;
+            while (rsOne.next()) {
+                dept = new Department(rsOne.getInt("department_num"),
+                        rsOne.getString("department_name"));
+            }
+
+            return dept;
+        }
+
+        public int insert() throws SQLException {
+            String insertDept = "INSERT INTO department (department_name) "
+                    + "values (?)";
+
+            PreparedStatement stmt = DatabaseManager.getConnection().prepareStatement(insertDept);
+            stmt.setString(1, this.getDept_name());
+
+            int count = stmt.executeUpdate();
+
+            return count;
+        }
+
+        /**
+         * @return the dept_number
+         */
+        public int getDept_number() {
+            return dept_number;
+        }
+
+        /**
+         * @param dept_number the dept_number to set
+         */
+        public void setDept_number(int dept_number) {
+            this.dept_number = dept_number;
+        }
+
+        /**
+         * @return the dept_name
+         */
+        public String getDept_name() {
+            return dept_name;
+        }
+
+        /**
+         * @param dept_name the dept_name to set
+         */
+        public void setDept_name(String dept_name) {
+            this.dept_name = dept_name;
+        }
+    }
+
+    /**
+     */
+    //public static void main(String[] args) {
+//        System.out.println("test");
+//        System.out.println("penis");
+//        System.out.println("big dawg");
+//        System.out.println("nashat was here");
+//    }
+    public class CSCI366 {
+
+        public void start() {
+            String anotherRequest = "yes";
+
+            while (anotherRequest.equalsIgnoreCase("yes")) {
+
+                Scanner scanChoice = new Scanner(System.in);
+                System.out.println("Enter 1 to manage department, "
+                        + "2 to manage employee");
+                int choice = scanChoice.nextInt();
+
+                switch (choice) {
+                    case 1 ->
+                        this.manageDepartment();
+                    case 2 ->
+                        this.manageEmployee();
+                    default ->
+                        System.out.println("Invalid operation.");
+                }
+
+                System.out.println("Another request? ");
+                Scanner scanRequest = new Scanner(System.in);
+                anotherRequest = scanRequest.next();
+            }
+        }
+
+        private void manageDepartment() {
+
+            System.out.println("Enter 1 to insert a new department"
+                    + ", enter 2 to select a department by name");
+
+            Scanner scan = new Scanner(System.in);
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 ->
+                    this.insertDepartment();
+                case 2 ->
+                    this.selectADepartmentByName();
+                default ->
+                    System.out.println("Invalid operation.");
+            }
+        }
+
+        private void manageEmployee() {
+
+        }
+
+        private void insertDepartment() {
+            System.out.print("Please provide the name of the new department:");
+            Scanner scan = new Scanner(System.in);
+            String name = scan.next();
+            Department d = new Department(name);
+
+            int count = 0;
+            try {
+                count = d.insert();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println(count + " department has been inserted.");
+        }
+
+        private void selectADepartmentByName() {
+            System.out.print("Please provide the name of the department:");
+            Scanner scan = new Scanner(System.in);
+            String name = scan.next();
+
+            Department d = null;
+            try {
+                d = Department.getDepartmentByName(name);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            if (d != null) {
+                System.out.println(d.getDept_number() + "-" + d.getDept_name());
+            } else {
+                System.out.println("This department does not exist.");
+            }
+        }
+
+        public static void main(String[] args) {
+            new CSCI366().start();
+        }
+
+    }
+
 }
