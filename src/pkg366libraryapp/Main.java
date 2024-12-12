623c1014b99aaec2a10465e70abd6fdbb1d6f934
rwetz <rwetz00@gmail.com>
2024-12-12 13:01:57 -0600

    Added stuff

--- src/pkg366libraryapp/Main.java
+++ src/pkg366libraryapp/Main.java
@@ -2,49 +2,13 @@ package pkg366libraryapp;
 
 import java.sql.*;
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 /**
  *
  * @author group
  */
 public class Main {
-
-    public class DatabaseManager {
-
-        private static String jdbcURL = "jdbc:postgresql://localhost:5432/LibraryAdmin";
-        private static String username = "postgres";
-        private static String password = "password12345!";
-
-        private static Connection connection;
-
-        public static Connection getConnection() {
-            if (connection == null) {
-                setConnection();
-            }
-            return connection;
-        }
-
-        private static void setConnection() {
-            try {
-                // load the driver
-                Class.forName("org.postgresql.Driver");
-                // connect to the database
-                connection
-                        = DriverManager.getConnection(jdbcURL, username, password);
-
-            } catch (ClassNotFoundException e) {
-                System.out.println("Cannot load the driver");
-                e.printStackTrace();
-            } catch (SQLException e) {
-                System.out.println("Got a SQL exception.");
-                e.printStackTrace();
-            }
-        }
-    }
 
     public class Department {
 
